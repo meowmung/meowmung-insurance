ac262b0edb8bc1f4f3644e8540268b5f6d6548b8
이승준 <104449960+lsjdg@users.noreply.github.com>
2024-11-14 17:15:37 +0900

    feat: 원문 참고 추천

--- bots/recommend.py
+++ bots/recommend.py
@@ -9,7 +9,7 @@ class RecommendBot:
             model_name=model_name, streaming=streaming, temperature=temperature
         )
         self.vectorstore = vectorstore
-        self.retriever = self.vectorstore.as_retriever(search_kwargs={"k": 3})
+        self.retriever = self.vectorstore.as_retriever(search_kwargs={"k": 2})
 
     def recommend(
         self,
@@ -21,32 +21,37 @@ class RecommendBot:
         concerned_illnesses,
     ):
 
-        question = (
-            "concerned_illnesses 배열에 나열된 질병들을 보장하는 특약들을 포함하는 "
-            "보험 3개를 추천해주세요."
-        )
+        illnesses_text = ", ".join(concerned_illnesses)
+
+        question = f"{illnesses_text} 을 보장하는 특약들을 포함하는 서로 다른 보험 2개를 추천해주세요. 각 보험에 해당하는 특약들도 함께 추천해주세요."
 
         result = self.retriever.get_relevant_documents(question)
         context = "\n".join([doc.page_content for doc in result])
 
         prompt = f"""
-        당신은 반려견과 반려묘를 위한 보험 상품을 추천하는 챗봇입니다.
-        사용자는 다음과 같은 특징을 입력했습니다:
-        - 반려동물 종류: {pet_type}
-        - 품종: {breed}
-        - 나이: {age}
-        - 성별: {gender}
-        - 중성화 여부: {neutered}
-        - 걱정되는 질병: {', '.join(concerned_illnesses)}
+        사용자가 입력한 정보는 다음과 같습니다:
+        - 동물 종류: {pet_type}, 품종: {breed}, 나이: {age}, 성별: {gender}, 중성화 여부: {neutered}
+        - 걱정되는 질병: {illnesses_text}
 
-        위 정보를 바탕으로, 제공된 문서에서 이 동물에게 적합한 보험 상품을 3개 추천해주세요.
-        각 보험에 대한 관련 특약만 포함하여 아래 JSON 형식으로 답변하세요:
+        주어진 정보를 바탕으로 적합한 보험 상품 2개를 추천해주세요.
+        보험 상품의 특약 중 details 에 걱정되는 질병에 대한 내용을 포함하는 것을 모두 검색하세요.
+        특약의 갯수에는 제한이 없습니다. 단, 관련 없는 특약은 가져와서는 안됩니다.
+        적합한 보험 상품은 위에 명시된 질병을 모두 보장할 수 있는 것입니다.
+        각 보험 상품의 특약 정보는 special_contracts 배열에 추가하세요.
+        걱정되는 질병에 관한 내용을 포함하는 특약들을 모두 포함하여 아래 형식으로 출력하세요:
         {{
-            "insurance": "보험사 이름",
-            "special_contracts": [context 에 주어진 json 형태의 각 특약의 세부정보]
+            "insurance": "보험상품 이름",
+            "special_contracts": [
+            {{
+                "name": 특약 이름,
+                "causes": 보험금 지급 사유,
+                "limits": 보장 한도,
+                "details": 특약 요약
+            }}
+            ]
         }}
-        답변을 ```json 등의 래퍼로 감싸지 마세요.
 
+        답변을 '''json 등의 래퍼로 감싸지 마세요
         {context}
         """
 
@@ -71,7 +76,7 @@ if __name__ == "__main__":
         age="4",
         gender="F",
         neutered="yes",
-        concerned_illnesses=["슬개골", "치과 치료"],
+        concerned_illnesses=["백내장", "슬개골"],
     )
 
     print(response.content)
--- loaders/dataloader.py
+++ loaders/dataloader.py
@@ -23,7 +23,7 @@ class Loader:
         if file_path:
             self.docs = self.load_file(file_path)
         elif dir_path:
-            self.docs = self.load_dir(dir_path)
+            self.docs = self.load_dir_with_metadata(dir_path)
         else:
             raise ValueError("Either 'dir_path' or 'file_path' must be provided.")
         if has_special_terms:
@@ -39,6 +39,26 @@ class Loader:
                     doc = Document(
                         page_content=json.dumps(data, ensure_ascii=False),
                         metadata={"source": filename},
+                        doc_id=filename,
+                    )
+                    docs.append(doc)
+        return docs
+
+    def load_dir_with_metadata(self, dir_path):
+        docs = []
+        for filename in os.listdir(dir_path):
+            if filename.endswith(".json"):
+                file_path = os.path.join(dir_path, filename)
+                with open(file_path, "r", encoding="utf-8") as f:
+                    data = json.load(f)
+                    doc = Document(
+                        page_content=json.dumps(
+                            data["special_terms"], ensure_ascii=False
+                        ),
+                        metadata={
+                            "company": data["company"],
+                            "insurance": data["insurance"],
+                        },
                         doc_id=filename,
                     )
                     docs.append(doc)
@@ -93,11 +113,20 @@ def extract_company_name(file_path):
 
 
 if __name__ == "__main__":
-    # loader = Loader(dir_path="summaries/cat", has_special_terms=False)
-    # print(loader.docs[1].page_content)
-    # loader.save_loader("data/dataloaders/cat_loader.pkl")
+    pet_types = ["dog", "cat"]
 
-    loader = load_loader("data/dataloaders/KB_dog_loader.pkl")
+    for type in pet_types:
+        loader = Loader(dir_path=f"summaries/{type}", has_special_terms=False)
+        print(loader.docs[1].page_content)
+
+    # --------debug-----------
+    # loader.save_loader("data/dataloaders/dog_loader.pkl")
+
+    # loader = load_loader("data/dataloaders/KB_dog_loader.pkl")
     # for i in range(len(loader.docs)):
     #     print(loader.docs[i].page_content)
-    print(loader.docs[0].metadata)
+    # print(loader.docs[0].metadata)
+
+    # --------debug add metadata-----------
+    # loader = Loader(dir_path="summaries/dog", has_special_terms=False)
+    # print(loader.docs[0].page_content)
