4a7ba58956ce7944fdf19461f6b57b35c9a5406c
이승준 <104449960+lsjdg@users.noreply.github.com>
2024-12-15 13:50:38 +0900

    feat: adminApp

--- adminPage/adminApp.py
+++ adminPage/adminApp.py
@@ -4,6 +4,7 @@ from bots.dataloader import *
 from dotenv import load_dotenv
 from bots.summary import *
 from bots.query import *
+import uvicorn
 
 load_dotenv()
 
@@ -39,3 +40,7 @@ async def save_summary(request: InfoRequest):
     except Exception as e:
         print(f"Error: {str(e)}")
         raise HTTPException(status_code=500, detail=str(e))
+
+
+if __name__ == "__main__":
+    uvicorn.run("adminPage.adminApp:app", host="0.0.0.0", port=8008, reload=True)
