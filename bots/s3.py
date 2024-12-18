979adff6f5acb0692d711830dfbb58ab59e53a2b
이승준 <104449960+lsjdg@users.noreply.github.com>
2024-12-19 00:36:52 +0900

    refactor: 백엔드와 요청 형식 동기화

--- bots/s3.py
+++ bots/s3.py
@@ -14,7 +14,7 @@ load_dotenv()
 aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
 aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
 region = os.getenv("AWS_DEFAULT_REGION")
-service = os.getenv("service")
+service = "s3"
 awsauth = AWS4Auth(aws_access_key, aws_secret_key, region, service)
 
 bucket_url = "https://hk-project-1.s3.ap-northeast-2.amazonaws.com/meowmung-insurance/"
